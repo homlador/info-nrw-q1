ea1686be5dbe030747ab407240c59d7c4b27b7e2
Benedikt Roth <rot@thg-dinslaken.de>
2015-05-01 17:57:12 +0200

    remove docs

--- Suchbaum/doc/runnings.js
+++
@@ -1,8 +0,0 @@
-exports.header = null
-
-exports.footer = {
-	height: "1cm",
-	contents: function(pageNum, numPages) {
-		return "<h4><span style='float:right'> Seite " + pageNum + " / " + numPages + "</span></h4>"
-	}
-}
