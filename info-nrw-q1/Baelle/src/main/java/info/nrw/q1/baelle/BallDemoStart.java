8281cb59bf278001fdcc99566be45d73f7a5474c
Benedikt Roth <rot@thg-dinslaken.de>
2015-08-04 15:15:03 +0200

    vorlage

--- info-nrw-q1/Baelle/src/main/java/info/nrw/q1/baelle/BallDemoStart.java
+++ info-nrw-q1/Baelle/src/main/java/info/nrw/q1/baelle/BallDemoStart.java
@@ -3,6 +3,6 @@ package info.nrw.q1.baelle;
 public class BallDemoStart {
 	public static void main(String[] args) {
 		BallDemo demo = new BallDemo();
-		demo.springenLassen();
+		demo.zeichneDemo();
 	}
 }
