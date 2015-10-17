6803bb623d2e6158ecb5f8fa510fb1230e50f4c5
Benedikt Roth <rot@thg-dinslaken.de>
2015-10-18 00:51:34 +0200

    bessere vorlage

--- info-nrw-q1/bahnhof/src/main/java/info/nrw/q1/bahnhof/Lok.java
+++ info-nrw-q1/bahnhof/src/main/java/info/nrw/q1/bahnhof/Lok.java
@@ -10,5 +10,50 @@ public class Lok {
 		gleisA = new Stack<Waggon>();
 		gleisB = new Stack<Waggon>();
 		gleisC = new Stack<Waggon>();
+		
+		gleisA.push(new Waggon(6));
+		gleisA.push(new Waggon(11));
+		gleisA.push(new Waggon(15));
+		gleisA.push(new Waggon(14));
 	}	
+
+	public void alleWaggonsueberpruefen() {
+		// TODO
+	}
+	
+	public void sortiertRangieren() {
+		// TODO
+	}
+	
+	private void ausgabe() {
+		ausgabe("GleisA: ", gleisA);
+		ausgabe("GleisB: ", gleisB);
+		ausgabe("GleisC: ", gleisC);
+	}
+
+	private void ausgabe(String s, Stack<Waggon> gleis) {
+		System.out.print(s);
+		
+		Stack<Waggon> temp = new Stack<Waggon>();		
+		while(gleis.isEmpty() == false) {
+			temp.push(gleis.top());
+			gleis.pop();
+		}
+		
+		while(temp.isEmpty() == false) {
+			Waggon w = temp.top();
+			System.out.print(w.gibNummer());
+			gleis.push(w);
+			temp.pop();
+			if (temp.isEmpty() == false) {
+				System.out.print(",");
+			}
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Lok l = new Lok();
+		l.ausgabe();
+	}
 }
